88f46a144351e4f4d5d9fdd26028189329c5595f
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-25 20:05:45 +0400

    configure SignUp.tsx file and add it to the routing system

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 const router = createBrowserRouter([
   {
     path: "/SignIn",
     element: <SignIn />,
+  },
+  {
+    path: "/SignUp",
+    element: <SignUp />,
   },
 ]);
 
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,9 @@
+const SignUp: React.FC = () => {
+  return (
+    <>
+      <div className="text-blue-100">Sig Up</div>
+    </>
+  );
+};
+
+export default SignUp;
