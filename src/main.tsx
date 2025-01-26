5709bd9f7d186622ffeab4759b2450d3f7d7fa8d
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-26 20:01:58 +0400

    configure Home.tsx file and add it to the routing system

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Home from "./layouts/Home";
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: "/SignUp",
     element: <SignUp />,
+  },
+  {
+    path: "/Home",
+    element: <Home />,
   },
 ]);
 
