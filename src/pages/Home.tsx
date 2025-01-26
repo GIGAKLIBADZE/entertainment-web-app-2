141f94c785eac3bf014d415adfce794004b04790
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-26 23:38:00 +0400

    fetch data from a local server

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,43 @@
+// import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Idata } from "../types/Types";
+
 const Home: React.FC = () => {
+  //   const { id } = useParams<{ id: string }>();
+
+  const [data, setData] = useState<Idata[] | null>(null);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(`http://localhost:3003/movie`);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong.");
+      }
+
+      const responseToJson = await response.json();
+      setData(responseToJson);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  console.log(data);
+  console.log("hi");
   return (
     <div>
       <div>
         <h2>Trending</h2>
-        <section></section>
+        <p></p>
+        <section>
+          {data?.map((item) => (
+            <p key={item.title}>{item.title}</p>
+          ))}
+        </section>
       </div>
       <div>
         <h2>Reccomended for you</h2>
--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -2,3 +2,23 @@ export interface Ierrors {
   emailError: boolean;
   passwordError: boolean;
 }
+
+export interface Idata {
+  title: string;
+  thumbnail: {
+    trending: {
+      small: string;
+      large: string;
+    };
+    regular: {
+      small: string;
+      medium: string;
+      large: string;
+    };
+  };
+  year: number;
+  category: string;
+  rating: string;
+  isBookmarked: boolean;
+  isTrending: boolean;
+}
