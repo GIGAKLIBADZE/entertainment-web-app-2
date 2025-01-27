c568333f21aa01129c96d7390d0391b82f138ed5
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-01-27 14:46:53 +0400

    add styles and correct positioning for the reccomended movies

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,17 +38,46 @@ const Home: React.FC = () => {
         <h2 className="text-[2rem] font-light leading-normal tracking-[-0.31px] text-[#fff]">
           Reccomended for you
         </h2>
-        <section className="mt-[2.4rem]">
-          {/* {data?.map((item) => (
-            <div key={item.title}>
-              <img src={item.thumbnail.regular.small} alt="Movie thumbnail" />
-            </div>
-          ))} */}
+        <section className="mt-[2.4rem] flex justify-center flex-wrap gap-x-[1.5rem] gap-y-[1.6rem]">
           {data
             ?.filter((item) => item.isTrending === false)
             .map((item) => (
               <div key={item.title}>
-                <img src={item.thumbnail.regular.small} alt="Thumbnail" />
+                <img
+                  src={item.thumbnail.regular.small}
+                  alt="Thumbnail"
+                  className="w-[16.4rem] h-[11rem] rounded-[8px] object-contain"
+                />
+                <section className="flex items-center gap-[0.7rem] mt-[0.8rem]">
+                  <small className="description">
+                    {item.year}{" "}
+                    <span className="opacity-[0.5] ml-[0.6rem]">•</span>
+                  </small>
+                  <div></div>
+                  <div className="flex items-center gap-[0.4rem]">
+                    <img
+                      src={
+                        item.category === "Movie"
+                          ? "/images/icon-nav-movies.svg"
+                          : "/images/icon-nav-tv-series.svg"
+                      }
+                      alt="Category"
+                      className={`w-[1rem]
+                        ${item.category === "Movie" ? "h-[1rem]" : "h-[0.7rem]"}
+                      `}
+                    />
+                    <small className="description">
+                      {item.category}{" "}
+                      <span className="opacity-[0.5] ml-[0.6rem]">•</span>
+                    </small>
+                  </div>
+
+                  <div></div>
+                  <small className="description">{item.rating}</small>
+                </section>
+                <p className="text-[1.4rem] font-medium leading-normal text-[#fff] mt-[0.6rem]">
+                  {item.title}
+                </p>
               </div>
             ))}
         </section>
