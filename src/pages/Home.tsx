d2b1e9aec9740cf0b329e81a83adab19324e6e32
GIGAKLIBADZE <giga.klibadze.1@btu.edu.ge>
2025-02-09 18:07:11 +0400

    correct positioning on the home page and styles for recommended pictures

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -13,7 +13,7 @@ const Home: React.FC = () => {
   const { data, toggleBookmark, search } = useContext(MainContext);
 
   return (
-    <div className="w-full">
+    <div className="xl:w-[91.6%]">
       <SearchArea />
       {search ? (
         <div>
@@ -21,27 +21,43 @@ const Home: React.FC = () => {
         </div>
       ) : (
         <div>
-          <div className="entertainment-container">
+          <div className="entertainment-container pr-0">
             <h2 className="title">Trending</h2>
-            <p></p>
             <section className="mt-[1.6rem]">
-              <Swiper spaceBetween={10} slidesPerView={"auto"}>
+              <Swiper
+                slidesPerView={"auto"}
+                spaceBetween={10}
+                breakpoints={{
+                  375: {
+                    slidesPerView: "auto",
+                    spaceBetween: 10,
+                  },
+                  768: {
+                    slidesPerView: "auto",
+                    spaceBetween: 20,
+                  },
+                  1280: {
+                    slidesPerView: "auto",
+                    spaceBetween: 20,
+                  },
+                }}
+              >
                 {data
                   ?.filter((item) => item.isTrending === true)
                   .map((item) => (
                     <SwiperSlide
-                      style={{ width: "24.5rem" }}
                       className="swiper-slide"
                       key={item.title}
+                      style={{ width: "70rem" }}
                     >
-                      <div className="relative parent">
+                      <div className="relative parent  xl:max-w-[47rem] slide-content md:w-[47rem] increase">
                         <img
                           src={item.thumbnail.regular.small}
                           alt="Thumbnail"
-                          className="w-[24rem] h-[14rem] rounded-[8px] relative"
+                          className="w-[24rem] h-[14rem] rounded-[8px] relative md:w-[47rem] md:h-[23rem]"
                         />
                         <div
-                          className="bookmark-container"
+                          className="bookmark-container absolute "
                           onClick={() => {
                             toggleBookmark(item.title);
                           }}
@@ -55,7 +71,7 @@ const Home: React.FC = () => {
                         </div>
                         <HoveredItem />
                         <div />
-                        <section className="flex items-center gap-[0.8rem] ml-[1.6rem] absolute top-[8.6rem]">
+                        <section className="flex items-center gap-[0.8rem] ml-[1.6rem] absolute top-[8.6rem] md:top-[15.4rem]">
                           <small className="description !text-[1.2rem]">
                             {item.year}{" "}
                             <span className="opacity-[0.5] ml-[0.8rem]">•</span>
@@ -90,7 +106,7 @@ const Home: React.FC = () => {
                             {item.rating}
                           </small>
                         </section>
-                        <p className="text-[1.4rem] font-medium leading-normal text-[#fff] mt-[0.4rem] ml-[1.6rem] absolute top-[10.5rem]">
+                        <p className="text-[1.4rem] font-medium leading-normal text-[#fff] mt-[0.4rem] ml-[1.6rem] absolute top-[10.5rem] md:top-[17.6rem]">
                           {item.title}
                         </p>
                       </div>
@@ -102,16 +118,16 @@ const Home: React.FC = () => {
           </div>
           <div className="entertainment-container">
             <h2 className="title">Recommended for you</h2>
-            <section className="items-container">
+            <section className="items-container ">
               {data
                 ?.filter((item) => item.isTrending === false)
                 .map((item) => (
                   <div key={item.title} className="relative w-full">
-                    <div className="relative parent">
+                    <div className="relative parent ">
                       <img
                         src={item.thumbnail.regular.large}
                         alt="Thumbnail"
-                        className="item-image"
+                        className="item-image max-w-[100%]"
                       />
                       <div
                         className="bookmark-container"
